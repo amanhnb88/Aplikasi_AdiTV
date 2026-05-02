39dc02c798a93badf6e6744165f0303049e7dc83
amanhnb88 <aldry.manu02@gmail.com>
2026-05-02 20:57:56 +0900

    Update CloudstreamModule.kt

--- android/app/src/main/java/com/aditv/CloudstreamModule.kt
+++ android/app/src/main/java/com/aditv/CloudstreamModule.kt
@@ -1,30 +1,78 @@
 package com.aditv
 
+import android.content.Context
+import android.util.Log
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
-import android.util.Log
+import dalvik.system.DexClassLoader
+import java.io.File
+import java.io.FileOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class CloudstreamModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    // Nama modul yang akan dipanggil dari JavaScript
     override fun getName(): String {
         return "Cloudstream"
     }
 
-    // Fungsi yang akan dipicu saat tombol download ditekan
     @ReactMethod
     fun loadPlugin(pluginName: String, pluginUrl: String, promise: Promise) {
-        Log.d("Cloudstream", "Menerima perintah download plugin: $pluginName")
+        Log.d("Cloudstream", "Mulai proses download plugin: $pluginName")
         
-        try {
-            // NANTI KITA TARUH LOGIKA DOWNLOAD & DEXCLASSLOADER DI SINI
-            
-            // Untuk sekarang, kita balas ke JavaScript kalau jembatannya sukses
-            promise.resolve("Jembatan Kotlin berhasil! Siap mendownload: $pluginName dari $pluginUrl")
-        } catch (e: Exception) {
-            promise.reject("ERROR", "Gagal memproses plugin", e)
+        // Kita jalankan di thread terpisah agar aplikasi tidak freeze/nge-hang saat download
+        thread {
+            try {
+                // 1. Siapkan folder tujuan di memori internal aplikasi
+                val pluginDir = File(reactApplicationContext.filesDir, "plugins")
+                if (!pluginDir.exists()) pluginDir.mkdirs()
+                
+                val outputFile = File(pluginDir, "$pluginName.cs3")
+
+                // 2. Proses Download File .cs3
+                val url = URL(pluginUrl)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.connect()
+
+                if (connection.responseCode != HttpURLConnection.HTTP_OK) {
+                    throw Exception("Server mengembalikan kode HTTP ${connection.responseCode}")
+                }
+
+                val inputStream = connection.inputStream
+                val outputStream = FileOutputStream(outputFile)
+                val buffer = ByteArray(1024)
+                var bytesRead: Int
+
+                while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                    outputStream.write(buffer, 0, bytesRead)
+                }
+
+                outputStream.close()
+                inputStream.close()
+                Log.d("Cloudstream", "Download selesai: ${outputFile.absolutePath}")
+
+                // 3. Proses "Ilmu Hitam" DexClassLoader untuk membaca .cs3
+                // File .cs3 sebenarnya adalah file .apk/.zip yang berisi classes.dex
+                val optimizedDexOutputPath = reactApplicationContext.getDir("outdex", Context.MODE_PRIVATE)
+                
+                val dexClassLoader = DexClassLoader(
+                    outputFile.absolutePath,
+                    optimizedDexOutputPath.absolutePath,
+                    null,
+                    reactApplicationContext.classLoader
+                )
+
+                // Jika sampai sini tidak error, berarti file .cs3 berhasil di-load ke dalam memori aplikasi!
+                promise.resolve("Plugin $pluginName berhasil didownload dan di-load ke mesin Android!")
+
+            } catch (e: Exception) {
+                Log.e("Cloudstream", "Error: ${e.message}")
+                promise.reject("PLUGIN_ERROR", "Gagal memproses plugin: ${e.message}", e)
+            }
         }
     }
 }
